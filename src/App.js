c01e2af46febbc3600c620221a066aa7d449f49c
Pragati215 <dhakanepragati4@gmail.com>
2021-03-11 19:50:35 +0530

    [Pragati]add: discovery app is created for the animation and transition property

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Experience from "./experience/experience"
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <Experience/>
+      </div>
+    );
+  }
+}
+
+export default App;
